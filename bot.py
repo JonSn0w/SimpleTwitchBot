14e385306399845a0c62d12b3536b81f2426bcab
JonSn0w <therealbrettstevenson@gmail.com>
2017-01-14 20:01:19 -0800

    :octocat: Initial commit

---
+++ bot.py
@@ -0,0 +1,65 @@
+# bot.py
+
+import re
+import cfg
+from time import sleep
+import socket
+
+# network functions go here
+
+s = socket.socket()
+s.connect((cfg.HOST, cfg.PORT))
+s.send("PASS {}\r\n".format(cfg.PASS).encode("utf-8"))
+s.send("NICK {}\r\n".format(cfg.NICK).encode("utf-8"))
+s.send("JOIN {}\r\n".format(cfg.CHAN).encode("utf-8"))
+
+CHAT_MSG=re.compile(r"^:\w+!\w+@\w+\.tmi\.twitch\.tv PRIVMSG #\w+ :")
+
+def Send_Message(message):
+    s.send("PRIVMSG #exo0tixz :" + message + "\r\n")
+
+while True:
+    response = s.recv(1024).decode("utf-8")
+    if response == "PING :tmi.twitch.tv\r\n":
+        s.send("PONG :tmi.twitch.tv\r\n".encode("utf-8"))
+    else:
+        username = re.search(r"\w+", response).group(0)
+        message = CHAT_MSG.sub("", response)
+        print(username + ": " + message)
+        if re.match("Hey", message):
+            Send_Message("Welcome to my stream, " + username)
+        for pattern in cfg.PATT:
+            if re.match(pattern, message):
+                # ban(s, username)
+                print('match')
+                break
+    time.sleep(1/1.5)
+
+
+def chat(sock, msg):
+    """
+    Send a chat message to the server.
+    Keyword arguments:
+    sock -- the socket over which to send the message
+    msg  -- the message to be sent
+    """
+    sock.send("PRIVMSG #{} :{}".format(cfg.CHAN, msg))
+
+def ban(sock, user):
+    """
+    Ban a user from the current channel.
+    Keyword arguments:
+    sock -- the socket over which to send the ban command
+    user -- the user to be banned
+    """
+    chat(sock, ".ban {}".format(user))
+
+def timeout(sock, user, secs=600):
+    """
+    Time out a user for a set period of time.
+    Keyword arguments:
+    sock -- the socket over which to send the timeout command
+    user -- the user to be timed out
+    secs -- the length of the timeout in seconds (default 600)
+    """
+    chat(sock, ".timeout {}".format(user, secs))
---
+++ cfg.py
@@ -0,0 +1,17 @@
+# cfg.py
+import re
+
+HOST = "irc.twitch.tv"                        # the Twitch IRC server
+PORT = 6667                                   # always use port 6667!
+NICK = "hello_murdoc"                         # your Twitch username, lowercase
+PASS = "oauth:..."                            # your Twitch OAuth token
+CHAN = "#hello_murdoc"                        # the channel you want to join
+RATE = 1.5                                    # messages per second
+
+PATT = [
+    r'(\d{3}[-\.\s]??\d{3}[-\.\s]??\d{4}|\(\d{3}\)\s*\d{3}[-\.\s]??\d{4}|\d{3}[-\.\s]??\d{4})',   # match phone numbers
+    re.compile(r'http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\(\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+', re.IGNORECASE),   # match links
+    re.compile(r"([a-z0-9!#$%&'*+\/=?^_`{|.}~-]+@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?)", re.IGNORECASE),  # match email addresses
+    r'(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)', # match IP addresses
+    re.compile(r'((?:(?:\\d{4}[- ]?){3}\\d{4}|\\d{15,16}))(?![\\d])', re.IGNORECASE),  # match credit card numbers
+]
